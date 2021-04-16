22ea853308ac9f79f5b155c402be58fd0a854121
Your Name <crrojasserrano@gmail.com>
2021-04-16 03:16:01 -0400

    agregadas Ãlas funcionalidades del contador

---
+++ server.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request ,redirect, session
+
+app = Flask(__name__)
+app.secret_key = 'clavesecreta' # asignar una clave secreta por motivos de seguridad
+
+# @app.route('/')
+# def index():
+#     print("*"*20)
+#     print("carga contador.html")
+#     return render_template("contador.html")
+
+
+@app.route('/')
+def refresh():
+    print("Got Post Info")
+    # verificar si ya está creada la sesión
+    if 'contador' not in session:
+        session['contador'] = 0
+    session['contador']+=1
+    return render_template('/contador.html')
+
+@app.route('/destroy_session')
+def destroy():
+    session.clear()
+    return redirect('/')
+
+@app.route('/counter_plus_two')
+def counter_plus_two():
+    session['contador']+=1
+    return redirect('/')
+
+@app.route('/increment_xtimes', methods = ['POST'])
+def increment_xtimes():
+    session['contador']+= int(request.form["increment"]) - 1
+    return redirect('/')
+
+# @app.route('/counter') 
+# def show():
+#     session['visitas'] =  int(session['visitas']) + 1
+#     print("*"*20)
+#     print(f"ha aumentado el número {session['visitas']}")
+#     return render_template('contador.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
